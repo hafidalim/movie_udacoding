90dcfc9471fb8390dd1604c27cfc2d4d88871ecb
Hafid Ali Mustaqim <hafidd456@gmail.com>
2019-09-30 09:48:37 +0700

    first commit

---
+++ app/src/main/java/com/hafid/movie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hafid.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
