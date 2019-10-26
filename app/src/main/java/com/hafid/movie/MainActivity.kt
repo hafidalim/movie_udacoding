74740092e728810f2bc38e733cccd11fd8ed9de1
Hafid Ali Mustaqim <55328717+hafidalim@users.noreply.github.com>
2019-10-26 23:49:17 +0700

    Update MainActivity.kt

--- app/src/main/java/com/hafid/movie/MainActivity.kt
+++ app/src/main/java/com/hafid/movie/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateeinit var presenter : LoginPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        presenter = LoginPresenter()
+        
     }
 }
